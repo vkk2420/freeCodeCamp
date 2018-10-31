8c5d2fa5e51c9b65c8a6372d3d7444c5aa2c0b25
vkk2420 <44321093+vkk2420@users.noreply.github.com>
2018-10-31 23:39:08 +0530

    Update SVG basic shapes using D3.js

--- guide/english/d3/SVG basic shapes using D3.js
+++ guide/english/d3/SVG basic shapes using D3.js
@@ -85,3 +85,65 @@ Two important things to pay attention to
 2. The SVG Coordinate system is in place so the height starts from the TOP and moves down.
 
 Notice that the important attributes we need to draw an SVG Rectangle in D3.js are - x, y, width and height.
+
+********************************************************************************************************************
+Drawing an SVG Straight Line using D3.js
+
+<svg width="50" height="50">
+ <line x1="5" y1="5" x2="40" y2="40" stroke="gray" stroke-width="5"  />
+</svg>
+
+We can probably guess that to build a straight line, you will need a "x1", "y1", "x2" and "y2".
+
+We can then make the process of drawing a straight line as two step process:
+1) Make an SVG container (the SVG Coordinate Space)
+2) Draw the straight line
+
+So in the simplest case, our JavaScript code would look like:
+
+//Make an SVG Container
+ var svgContainer = d3.select("body").append("svg")
+                                     .attr("width", 200)
+                                     .attr("height", 200);
+ 
+ //Draw the line
+ var circle = svgContainer.append("line")
+                          .attr("x1", 5)
+                          .attr("y1", 5)
+                         .attr("x2", 50)
+                         .attr("y2", 50);
+(Wait a second?! Where is the line????? The SVG element is there, however we cannot see it in our browser.
+It turns out because the SVG ‘line’ elements are single lines and thus are geometrically one-dimensional, that is - they have no interior.
+
+Which means that ‘line’ elements are never filled (see the ‘fill’ property).
+Which means that our line does not take up space - so we do not actually see anything.
+
+To fix this, make sure to give the line:
+
+.attribute("stroke-width", NUMBER), where NUMBER is how wide the line is in units.
+.attribute("stroke", "COLOR"), where COLOR is a color to used to color the line.
+
+So fixing our simplest case, our JavaScript code would look like:
+        //Make an SVG Container
+ var svgContainer = d3.select("body").append("svg")
+                                     .attr("width", 200)
+                                     .attr("height", 200);
+ 
+ //Draw the line
+ var circle = svgContainer.append("line")
+                          .attr("x1", 5)
+                          .attr("y1", 5)
+                         .attr("x2", 50)
+                         .attr("y2", 50)
+                         .attr("stroke-width", 2)
+                         .attr("stroke", "black");
+
+Fantastic - now the line is visible!
+
+The necessary SVG attributes for drawing a straight line are the "x1", "y1", "x2", "y2", "stroke" and "stroke-width".
+Note - We don't use a style method with the line. Because ‘line’ elements are single lines and thus are geometrically one-dimensional, 
+       they have no interior. Which is why to style them we need to deal with the "stroke" color and "stroke-width".
+Notice that the important attributes we need to draw an SVG Straight Line in D3.js are - x1, y1, x2, y2, stroke-width and stroke.
+
+****************************************************************************************************************************************
+
